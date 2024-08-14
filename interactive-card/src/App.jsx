dbffbe981f64d6fe931a18fed914e478cede5362
luan048 <luanmonteiropedrosa@gmail.com>
2024-08-13 23:23:00 -0300

    Add Initial Archive

---
+++ interactive-card/src/App.jsx
@@ -0,0 +1,50 @@
+import imgSideLeft from '../public/images/bg-main-desktop.png'
+import imgCardFront from '../public/images/bg-card-front.png'
+import imgCardBack from '../public/images/bg-card-back.png'
+import cardLogo from '../public/images/card-logo.svg'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='general'>
+        <div className='leftColum'>
+
+          <img src={imgSideLeft} className='imgSide' />
+
+          <div className='divImgCards'>
+            <div className='divCardFront'>
+              <img src={imgCardFront} className='cardFront' />
+              <img src={cardLogo} className='cardLogo' />
+            </div>
+            <img src={imgCardBack} className='cardBack' />
+          </div>
+        </div>
+
+        <div className='rightColum'>
+
+          <div className='divForm'>
+
+            <div className='elements'>
+              <i>CARDHOLDER NAME</i>
+              <input type="text" placeholder='e.g. Jane Appleseed' />
+            </div>
+
+            <div className='elements'>
+              <i>CARD NUMBER</i>
+              <input type="text" placeholder='e.g 1234 5678 9123 0000' />
+            </div>
+
+            {/* FALTA COLOCAR OS ELEMENTOS DE NÚMEROS DOS CARTÕES, ESTAVA COM ESTRUTURA HTML E CSS MAL FEITOS */}
+
+            <button className='button'>Confirm</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
